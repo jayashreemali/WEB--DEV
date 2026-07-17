223238c19adad35075b139aa678905bd5109a0a8
Jayshree Mali <jayashreemali171@gmail.com>
2026-07-17 10:21:50 +0530

    chenge welcome messag

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 
 # Title and description
-st.title("🚀 Welcome to Streamlit")
+st.title("🚀 Welcome to Streamlit.my first app")
 st.write("Build interactive web apps using only Python!")
 
 # Sidebar
